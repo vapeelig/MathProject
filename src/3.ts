b9c5b949f6f4aff3b8d5d0ff98a4e8245f13504e
vapeelig <vapeelig@gmail.com>
2025-03-08 20:29:24 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,12 @@
+
+import { getRandomInt } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MathProject';
+  number = getRandomInt(1, 10);
+}
