6e116b10811ed6320f9ea90ab95184af1b1c74a7
vapeelig <vapeelig@gmail.com>
2025-03-11 03:09:08 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,17 @@
+const MathProject = {
+  sum(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+  multiply(a: number, b: number): number {
+    return a * b;
+  },
+  divide(a: number, b: number): number {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+  },
+};
