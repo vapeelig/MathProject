331f4d73f90f843881a93e96f162c63bb69f3105
vapeelig <vapeelig@gmail.com>
2025-03-19 22:22:12 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,19 @@
+export class MathProject {
+  constructor() {}
+
+  public sum(a: number, b: number): number {
+    return a + b;
+  }
+
+  public subtract(a: number, b: number): number {
+    return a - b;
+  }
+
+  public multiply(a: number, b: number): number {
+    return a * b;
+  }
+
+  public divide(a: number, b: number): number {
+    return a / b;
+  }
+}
