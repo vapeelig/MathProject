0c2b83f7d087386e5859a1b6858716ae4cfede75
vapeelig <vapeelig@gmail.com>
2025-03-20 03:22:28 +0000

    Add 15.ts

---
+++ src/15.ts
@@ -0,0 +1,22 @@
+const mathProject = {
+  name: 'MathProject',
+  description: 'A repository for all Math related projects',
+  files: [
+    {
+      name: 'math.ts',
+      content: `export const add = (a: number, b: number): number => a + b;`,
+    },
+    {
+      name: 'subtract.ts',
+      content: `export const subtract = (a: number, b: number): number => a - b;`,
+    },
+    {
+      name: 'multiply.ts',
+      content: `export const multiply = (a: number, b: number): number => a * b;`,
+    },
+    {
+      name: 'divide.ts',
+      content: `export const divide = (a: number, b: number): number => a / b;`,
+    },
+  ],
+};
