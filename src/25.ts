7cd913e9c7ca4efa21977cc3c20979225eea5073
vapeelig <vapeelig@gmail.com>
2025-04-05 05:40:35 +0000

    Add 25.ts

---
+++ src/25.ts
@@ -0,0 +1,10 @@
+function main() {
+  // Example code to demonstrate how to use a specific function
+
+  const num1 = 5;
+  const num2 = 3;
+
+  console.log(`The sum of ${num1} and ${num2} is: ${num1 + num2}`);
+}
+
+main();
