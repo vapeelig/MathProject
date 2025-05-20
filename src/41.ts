815cc29224c920d9da0c3fb77425424214af7ac3
vapeelig <vapeelig@gmail.com>
2025-05-20 13:46:01 +0000

    Add 41.ts

---
+++ src/41.ts
@@ -0,0 +1,11 @@
+// This is a sample TypeScript code to demonstrate how to create a matrix. Note that this code does not include all the necessary aspects of creating and using matrices as part of a math project.
+// In a real-world application, you would need to import appropriate libraries such as `math.js` for handling matrices.
+
+import { Matrix } from 'mathjs';
+
+const matrix = new Matrix([
+  [1, 2],
+  [3, 4]
+]);
+
+console.log(matrix);
